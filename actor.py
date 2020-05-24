Model adjustments, fixed zero action gradient

actor.py
@@ -6,6 +6,8 @@ tf.keras.backend.set_floatx('float64')
 Model = tf.keras.Model
 Input = tf.keras.layers.Input
 Dense = tf.keras.layers.Dense
+Add = tf.keras.layers.Add
+Multiply = tf.keras.layers.Multiply
 BatchNormalization = tf.keras.layers.BatchNormalization
 Activation = tf.keras.layers.Activation
 
@@ -24,19 +26,39 @@ class Actor:
         self.state_dim = env.observation_space.shape[0]
         self.action_dim = env.action_space.shape[0]
 
+        action_center = (self.env.action_space.high - self.env.action_space.low) / 2
+        action_amplitude = (self.env.action_space.high + self.env.action_space.low) / 2
+        self.action_center = tf.convert_to_tensor(action_center.reshape((1, 36)))
+        self.action_amplitude = tf.convert_to_tensor(action_amplitude.reshape((1, 36)))
+
         self.model = self.create_actor_network()
         self.target_model = self.create_actor_network()
 
         self.optimizer = Adam(learning_rate=self.learning_rate)
 
     def create_actor_network(self):
-        inputs = Input(shape=(self.state_dim,))
-        hidden_1 = Dense(400, activation='relu')(inputs)
-        hidden_2 = Dense(300, activation='relu')(hidden_1)
-        hidden_3 = Dense(400, activation='relu')(hidden_2)
-        output = Dense(self.action_dim, activation='relu', )(hidden_3)
+        inputs = Input(shape=(self.state_dim,), name='actor_inputs')
+        #version 1
+        #hidden_1 = Dense(400, activation='relu', name='actor_hidden_1')(inputs)
+        #hidden_2 = Dense(300, activation='relu', name='actor_hidden_2')(hidden_1)
 
-        model = Model(inputs=inputs, outputs=output)
+        #version 2
+        hidden_1 = Dense(400, use_bias=False)(inputs)
+        hidden_2 = BatchNormalization()(hidden_1)
+        hidden_3 = Activation('relu')(hidden_2)
+        hidden_4 = Dense(300, use_bias=False)(hidden_3)
+        hidden_5 = BatchNormalization()(hidden_4)
+        hidden_6 = Activation('relu')(hidden_5)
+
+        weights_initializer = tf.keras.initializers.RandomUniform(minval=-0.003, maxval=0.003)
+        output = Dense(self.action_dim, activation='tanh', kernel_initializer=weights_initializer,
+                       bias_initializer=weights_initializer, name='actor_output')(hidden_6)
+
+        #scaling output range [-1, 1] to action range [env.action_space.low, self.env.action_space.high]
+        scaled_output = Multiply(name='actor_scaled_output')([output, self.action_amplitude])
+        translated_output = Add(name='actor_translated_output')([scaled_output, self.action_center])
+
+        model = Model(inputs=inputs, outputs=translated_output)
         optimizer = Adam(lr=0.001)
         model.compile(loss='mse', optimizer=optimizer)
 
@@ -52,8 +74,8 @@ class Actor:
     def train(self, cur_state, action_gradients):
         with tf.GradientTape() as tape:
             out = tf.reshape(self.model(cur_state, training=True), (1, self.action_dim))
-        actor_gradients = tape.gradient(out, self.model.trainable_variables, -action_gradients)
-        self.optimizer.apply_gradients(zip(actor_gradients, self.model.trainable_variables))
+        actor_gradients = tape.gradient(out, self.model.trainable_weights, -action_gradients)
+        self.optimizer.apply_gradients(zip(actor_gradients, self.model.trainable_weights))
 
     @tf.function
     def predict(self, inputs):
@@ -65,9 +87,9 @@ class Actor:
 
     @tf.function
     def update_target_network(self):
-        for i in range(len(self.target_model.trainable_variables)):
-            self.target_model.trainable_variables[i].assign(
-            tf.math.multiply(self.model.trainable_variables[i], self.tau)
-            + tf.math.multiply(self.target_model.trainable_variables[i], 1. - self.tau))
+        for i in range(len(self.target_model.trainable_weights)):
+            self.target_model.trainable_weights[i].assign(
+            tf.math.multiply(self.model.trainable_weights[i], self.tau)
+            + tf.math.multiply(self.target_model.trainable_weights[i], 1. - self.tau))
 
 

actor_critic.py
@@ -1,6 +1,4 @@
-"""
-solving pendulum using actor-critic model
-"""
+import os
 
 import gym
 
@@ -12,6 +10,17 @@ import pybullet_envs  # do not delete, necessary to make gym env
 from actor import Actor
 from buffer import Buffer
 from critic import Critic
+
+# TODO enable processing whole batch at once
+
+batch_size = 32
+
+restore_epoch = 4  # set to -1 when training without restoring weights
+make_checkpoints = True  # whether to save models' weights so that they could be restored later
+render = False  # whether to display humanoid model
+
+checkpoint_path = "../checkpoints/training_1/{model}/{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
 
 
 def run_actor_training(samples, critic, actor):
@@ -27,24 +36,19 @@ def run_critic_training(samples, critic, actor):
         cur_state, action, reward, new_state, done = sample
         if not done:
             target_action = actor.target_predict(new_state)
-            future_reward = critic.target_predict(new_state, target_action)[0][0]
+            future_reward = critic.target_predict(new_state, target_action)[0][0].numpy()
             reward += critic.gamma * future_reward
-            reward = reward.numpy()
-        critic.model.fit([cur_state, action], np.asarray([reward]), verbose=0)
+        critic.model.fit([cur_state, action], np.array([reward]), verbose=0)
 
 
 def run_training(actor, critic, buffer):
-    batch_size = 32
-    if len(buffer.memory) < batch_size:
-        return
-
     samples = random.sample(buffer.memory, batch_size)
     run_critic_training(samples, critic, actor)
     run_actor_training(samples, critic, actor)
 
 
 def main():
-    env = gym.make("HumanoidDeepMimicWalkBulletEnv-v1")
+    env = gym.make('HumanoidDeepMimicWalkBulletEnv-v1')
 
     learning_rate = 0.001
     epsilon = 1.0
@@ -56,16 +60,26 @@ def main():
     critic = Critic(env, learning_rate, epsilon, epsilon_decay, gamma, tau)
     buffer = Buffer()
 
-    num_trials = 10000
-    trial_len = 500
+    if restore_epoch >= 0:
+        actor.model.load_weights(checkpoint_path.format(model='actor_model', epoch=restore_epoch))
+        actor.target_model.load_weights(checkpoint_path.format(model='actor_target', epoch=restore_epoch))
+        critic.model.load_weights(checkpoint_path.format(model='critic_model', epoch=restore_epoch))
+        critic.target_model.load_weights(checkpoint_path.format(model='critic_target', epoch=restore_epoch))
 
-    # env.render()
-    for i in range(num_trials):
+    num_epochs = 50000
+    epoch_len = 500
 
-        print("Epoch {}".format(i))
+    if render:
+        env.render()
+
+    for epoch in range(num_epochs):
+
+        print("Epoch {}".format(epoch))
         cur_state = env.reset()
 
-        for j in range(trial_len):
+        max_reward = 0
+
+        for j in range(epoch_len):
 
             cur_state = cur_state.reshape((1, env.observation_space.shape[0]))
             action = actor.act(cur_state)
@@ -75,15 +89,26 @@ def main():
             new_state = new_state.reshape((1, env.observation_space.shape[0]))
             buffer.remember(cur_state, action, reward, new_state, done)
 
-            run_training(actor, critic, buffer)
+            if len(buffer.memory) > batch_size:
+                run_training(actor, critic, buffer)
 
-            cur_state = new_state
+                cur_state = new_state
 
-            actor.update_target_network()
-            critic.update_target_network()
+                actor.update_target_network()
+                critic.update_target_network()
+
+                max_reward = max(max_reward, reward)
 
             if done:
                 break
+
+        print('Max_reward {}'.format(max_reward))
+
+        if make_checkpoints:
+            actor.model.save_weights(checkpoint_path.format(model='actor_model', epoch=epoch))
+            actor.target_model.save_weights(checkpoint_path.format(model='actor_target', epoch=epoch))
+            critic.model.save_weights(checkpoint_path.format(model='critic_model', epoch=epoch))
+            critic.target_model.save_weights(checkpoint_path.format(model='critic_target', epoch=epoch))
 
 
 if __name__ == "__main__":

critic.py
@@ -6,6 +6,7 @@ Model = tf.keras.Model
 Input = tf.keras.layers.Input
 Dense = tf.keras.layers.Dense
 Add = tf.keras.layers.Add
+Lambda = tf.keras.layers.Lambda
 BatchNormalization = tf.keras.layers.BatchNormalization
 Activation = tf.keras.layers.Activation
 
@@ -28,20 +29,29 @@ class Critic:
         self.target_model = self.create_critic_network()
 
     def create_critic_network(self):
-        state = Input(shape=(self.state_dim,))
-        state_hidden_1 = Dense(400, activation='relu')(state)
-        state_hidden_2 = Dense(300, activation='relu')(state_hidden_1)
+        state = Input(shape=(self.state_dim,), name='critic_state_input')
+        #version 1
+        #state_hidden_1 = Dense(400, activation='relu', name='critic_state_hidden_1')(state)
 
-        action = Input(shape=(self.action_dim,))
-        action_hidden_1 = Dense(300)(action)
+        #version 2
+        state_hidden_1 = Dense(400, use_bias=False)(state)
+        state_hidden_2 = BatchNormalization()(state_hidden_1)
+        state_hidden_3 = Activation('relu')(state_hidden_2)
 
-        merged = Add()([state_hidden_2, action_hidden_1])
-        merged_hidden_1 = Dense(300, activation='relu')(merged)
+        state_hidden_2 = Dense(300, use_bias=False, name='critic_state_hidden_2')(state_hidden_3)
 
-        output = Dense(1, activation='relu')(merged_hidden_1)
+        action = Input(shape=(self.action_dim,), name='critic_action_input')
+        action_hidden_1 = Dense(300, name='critic_action_hidden_1')(action)
+
+        merged = Add(name='critic_merged')([state_hidden_2, action_hidden_1])
+        merged_hidden_1 = Activation('relu', name='critic_merged_hidden_1')(merged)
+
+        weights_initializer = tf.keras.initializers.RandomUniform(minval=-0.003, maxval=0.003)
+        output = Dense(1, kernel_initializer=weights_initializer, bias_initializer=weights_initializer,
+                       name='critic_output')(merged_hidden_1)
         model = Model(inputs=[state, action], outputs=output)
 
-        optimizer = Adam(lr=0.001)
+        optimizer = Adam(lr=0.1)
         model.compile(loss='mse', optimizer=optimizer)
 
         return model
@@ -58,13 +68,12 @@ class Critic:
     def action_gradients(self, state, action):
         with tf.GradientTape() as tape:
             tape.watch(action)
-            tape.watch(state)
-            out = self.model([state, action], training=True)
+            out = self.model([state, action])
         return tape.gradient(out, action)
 
     @tf.function
     def update_target_network(self):
-        for i in range(len(self.target_model.trainable_variables)):
-            self.target_model.trainable_variables[i].assign(
-                tf.math.multiply(self.model.trainable_variables[i], self.tau)
-                + tf.math.multiply(self.target_model.trainable_variables[i], 1. - self.tau))
+        for i in range(len(self.target_model.trainable_weights)):
+            self.target_model.trainable_weights[i].assign(
+                tf.math.multiply(self.model.trainable_weights[i], self.tau)
+                + tf.math.multiply(self.target_model.trainable_weights[i], 1. - self.tau))